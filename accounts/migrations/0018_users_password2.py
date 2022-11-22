d3b298dd0a8d372ccc218e3044472d7087c45f3c
michael-j-rubenstein <74865492+michael-j-rubenstein@users.noreply.github.com>
2022-11-21 19:16:24 -0500

    Update and rename 0017_users_password2.py to 0018_users_password2.py

--- accounts/migrations/0018_users_password2.py
+++ accounts/migrations/0018_users_password2.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("accounts", "0016_users_class_lst_users_classes"),
+        ("accounts", "0017_users_class_lst_users_classes"),
     ]
 
     operations = [
