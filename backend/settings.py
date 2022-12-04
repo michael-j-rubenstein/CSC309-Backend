e559958410895fe513cd33e7f17e56756bd558c1
Michael J R <michael.rubenstein@mail.utoronto.ca>
2022-12-03 23:02:11 -0500

    allowed cors

--- backend/settings.py
+++ backend/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'corsheaders',
     'rest_framework',
     "rest_framework.authtoken",
     'studios',
@@ -57,7 +57,14 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
+
+CORS_ORIGIN_ALLOW_ALL = True
+
+ALLOWED_HOSTS = ['*']
+
+CORS_ALLOWED_ORIGINS = ['http://127.0.0.1:3000', 'http://127.0.0.1:8000']
 
 ROOT_URLCONF = 'backend.urls'
 
