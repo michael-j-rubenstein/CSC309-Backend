ab78e787a335b316e60ab939cb15d5da2074091e
ayanaarahman <ayanaa.rahman@mail.utoronto.ca>
2022-12-08 01:50:09 -0500

    users can now edit their passwords

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,10 +1,10 @@
 from django.contrib.auth.password_validation import validate_password
+from django.shortcuts import get_object_or_404
 from rest_framework import serializers
 from accounts.models import Users
 
 
 class SignupSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = Users
         fields = ('username', 'password', 'email', 'first_name', 'last_name', 'avatar', 'phone_number', 'password2')
@@ -23,6 +23,26 @@ class SignupSerializer(serializers.ModelSerializer):
 
 
 class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Users
+        fields = ('email', 'first_name', 'last_name', 'avatar', 'phone_number', 'password', 'password2')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"Error: The two password fields didn't match"})
+
+        return attrs
+
+    def save(self, user_id):
+        user = get_object_or_404(Users, pk=user_id)
+        if self.validated_data['password'] != '' and self.validated_data['password2'] != '':
+            user.set_password(self.validated_data['password'])
+            user.save()
+        user.save()
+        return user
+
+
+class MePageSerializer(serializers.ModelSerializer):
     class Meta:
         model = Users
         fields = ('email', 'first_name', 'last_name', 'avatar', 'phone_number')
